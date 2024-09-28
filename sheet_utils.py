9bfc296e5f085614e1845f5e9e93ea7945bc7452
Fernando Borrego <fernando.borrego@gmail.com>
2024-09-27 23:02:24 -0600

    SHEET update

--- sheet_utils.py
+++ sheet_utils.py
@@ -1,5 +1,5 @@
 import pprint # noqa
-from utils import get_f1
+# from utils import get_f1
 
 
 def get_hum_fecha(fecha):
@@ -38,7 +38,7 @@ def write_sheet_match(wks, match):
     liga = match['liga']
     home = match['home']
     away = match['away']
-    promedio_gol = match['promedio_gol']
+    # promedio_gol = match['promedio_gol']
     home_matches = match['home_matches']['matches']
     home_ft_1 = home_matches[0]['ft']
     home_ft_2 = home_matches[1]['ft']
@@ -49,7 +49,7 @@ def write_sheet_match(wks, match):
     home_concedidos = match['home_matches']['concedidos']
     home_p_hechos = match['home_matches']['p_hechos']
     home_p_concedidos = match['home_matches']['p_concedidos']
-    home_35 = f'{match['home_matches']['p35'] * 100}%'
+    home_35 = f'{match["home_matches"]["p35"] * 100}%'
     away_matches = match['away_matches']['matches']
     away_ft_1 = away_matches[0]['ft']
     away_ft_2 = away_matches[1]['ft']
@@ -60,17 +60,17 @@ def write_sheet_match(wks, match):
     away_concedidos = match['away_matches']['concedidos']
     away_p_hechos = match['away_matches']['p_hechos']
     away_p_concedidos = match['away_matches']['p_concedidos']
-    away_35 = f'{match['away_matches']['p45'] * 100}%'
+    away_35 = f'{match["away_matches"]["p45"] * 100}%'
     face_matches = match['face_matches']['matches']
     face_ft_1, face_ft_2, face_ft_3, face_ft_4, face_ft_5 = (
         (face_matches[n]['ft'] if n < len(face_matches) else '') for n in range(5) # noqa
     )
-    face_35 = f'{match['face_matches']['p35'] * 100}%'
+    face_35 = f'{match["face_matches"]["p35"] * 100}%'
     momio_home = match['momio_home'] if 'momio_home' in match else ''
     momio_away = match['momio_away'] if 'momio_away' in match else ''
-    dif_momio_win = ''
-    if momio_home != '-' and momio_away != '':
-        dif_momio_win = int(momio_home) - int(momio_away)
+    # dif_momio_win = ''
+    # if momio_home != '-' and momio_away != '':
+    #     dif_momio_win = int(momio_home) - int(momio_away)
     momio_si = match['momio_si'] if 'momio_si' in match else ''
     momio_no = match['momio_no'] if 'momio_no' in match else ''
     dif_momio_sino = ''
@@ -86,14 +86,15 @@ def write_sheet_match(wks, match):
     momio_ft_45 = match['momio_ft_45'] if 'momio_ft_45' in match else ''
     usuario = match['usuario'] if 'usuario' in match else ''
     revision = match['revision'] if 'revision' in match else ''
-    f1 = get_f1(dif_momio_sino)
+    # f1 = get_f1(dif_momio_sino)
     reg = [
         fecha[:10],
         hora,
         home,
         away,
-        '',
-        '',
+        '',  # AP
+        '',  # X1
+        '',  # X2
         pais,
         liga,
         home_ft_1,
@@ -106,7 +107,8 @@ def write_sheet_match(wks, match):
         '5',  # no juegos Local
         home_p_hechos,
         home_p_concedidos,
-        away_ft_1,
+        '',  # X3
+        away_ft_1,  # U
         away_ft_2,
         away_ft_3,
         away_ft_4,
@@ -116,44 +118,43 @@ def write_sheet_match(wks, match):
         '5',  # no juegos Visitante
         away_p_hechos,
         away_p_concedidos,
-        face_ft_1,
+        face_ft_1,  # AE
         face_ft_2,
         face_ft_3,
         face_ft_4,
         face_ft_5,
-        momio_home,
+        momio_home,  # AJ
         momio_away,
-        dif_momio_win,
-        momio_si,
+        momio_si,  # AL
         momio_no,
         dif_momio_sino,
-        momio_ht_05,
+        momio_ht_05,  # AO
         momio_ht_15,
         momio_ht_25,
-        momio_ft_05,
+        momio_ft_05,  # AR
         momio_ft_15,
         momio_ft_25,
         momio_ft_35,
         momio_ft_45,
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
-        promedio_gol,
-        home_35,
+        '',  # -5.5  AW
+        '',  # linea de gol 1  AX
+        '',  # linea de gol 2  AY
+        '',  # linea de gol 3  AZ
+        '',  # linea de gol 4  BA
+        '',  # ROJA l BB
+        '',  # ROJA V BC
+        home_35,  # BD
         away_35,
         face_35,
-        '',  # Total
-        f1,  # f1
-        '',  # f2
-        '',  # f3
-        '',  # f4
-        '',  # f5
-        '',  # f6
-        '',  # Observacion
+        '',  # Total BG
+        '',  # Observacion BH
+        '',  # f1 BI
+        '',  # f2 BJ
+        '',  # f3 BK
+        '',  # f4 BL
+        '',  # f5 BM
+        '',  # f6 BN
+        '',  # AP BO
         revision,  # Revision
         usuario,  # Usuario
         url  # link totalcorner
