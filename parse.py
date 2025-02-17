bba1d1e937ac3078de09b4a2f556346a0efbbaf4
Fernando Borrego <fernando.borrego@gmail.com>
2025-02-17 06:48:46 -0600

    resultados fix

--- parse.py
+++ parse.py
@@ -57,22 +57,24 @@ def get_marcador_ft(web, debug=False):
         if len(eventos_away) > 0:
             for evento in eventos_away:
                 minuto = evento.find('div', class_='smv__timeBox').text.strip().replace("'", '') # noqa
-                minuto = int(minuto) if '+' not in minuto else int(minuto[:2]) # noqa
-                icono = evento.find('div', class_='smv__incidentIcon') # noqa
-                if icono:
-                    svg = icono.find('svg')
-                    if svg:
-                        incident_icon = svg.get('class')
-                        # incident_icon_goal = svg.get('data-testid') # noqa
-                        if 'card-ico' in incident_icon:
-                            if 'yellowCard-ico' not in incident_icon: # noqa
-                                rojas_away.append([minuto, 'Away']) # noqa
-                        else:
-                            gol_text = icono.text.lower()
-                            if debug:
-                                print('GOAL Away', gol_text, minuto)
-                            if all([x not in gol_text for x in goles_fallos]):
-                                goles.append([minuto, 'Away']) # noqa
+                print(f"'{minuto}'", f"'{minuto[:2]}'", f"'{evento.find('div', class_='smv__timeBox').text}'")
+                if minuto != '':
+                    minuto = int(minuto) if '+' not in minuto else int(minuto[:2]) # noqa
+                    icono = evento.find('div', class_='smv__incidentIcon') # noqa
+                    if icono:
+                        svg = icono.find('svg')
+                        if svg:
+                            incident_icon = svg.get('class')
+                            # incident_icon_goal = svg.get('data-testid') # noqa
+                            if 'card-ico' in incident_icon:
+                                if 'yellowCard-ico' not in incident_icon: # noqa
+                                    rojas_away.append([minuto, 'Away']) # noqa
+                            else:
+                                gol_text = icono.text.lower()
+                                if debug:
+                                    print('GOAL Away', gol_text, minuto)
+                                if all([x not in gol_text for x in goles_fallos]):
+                                    goles.append([minuto, 'Away']) # noqa
 
         total_goles = str(len(goles))
         goles_ordenados = sorted(goles, key=lambda x: x[0])
@@ -1015,13 +1017,15 @@ def parse_handicap(html):
 if __name__ == '__main__':
     web = Web(multiples=True)
     ids = [
-        'viwDtPCi',
-        'n7eyYwcJ',
-        'xxxgL0DD',  # PENALTI FALLADO
-        'vsOtJ9xh'  # sumo goles de mas
+        # 'viwDtPCi',
+        # 'n7eyYwcJ',
+        # 'xxxgL0DD',  # PENALTI FALLADO
+        # 'vsOtJ9xh',  # sumo goles de mas
+        'UipXRFJ6'
     ]
     for partido_id in ids:
         link = f'https://www.flashscore.com.mx/partido/{partido_id}/?d=1#/resumen-del-partido/resumen-del-partido' # noqa
+        print(link)
         web.open(link) # noqa 
         web.wait(1)
         print(partido_id)
