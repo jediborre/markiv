15f345c6fec03d2beb1a595cd64ac960813bc480
Fernando Borrego <fernando.borrego@gmail.com>
2025-03-04 12:27:43 -0600

    deleteold  model and dbs

--- model/__init__.py
+++
@@ -1,5 +0,0 @@
-from .match import Match # noqa
-from .match_stats import MatchStats # noqa
-from .match_stats_home_matches import MatchStatsHomeMatches # noqa
-from .match_stats_away_matches import MatchStatsAwayMatches # noqa
-from .match_stats_face_matches import MatchStatsFaceMatches # noqa
